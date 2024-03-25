c89436edc743a7b1c357e722f7e4b9d69493205c
ulzidorj <147369501+ulzidorj@users.noreply.github.com>
2024-03-24 21:14:08 -0700

    commit 3.24

---
+++ js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $("#ViewProject").click(function () {
+    $(".workGrid").slideDown(1000); // Slides down the content over 1 second (1000 milliseconds)
+  });
+});
